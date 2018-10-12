056f119e18f5f1c98d7c54333aa5da88dd533059
Vincze Szilvia <sz1lv.vincze@gmail.com>
2018-10-12 15:36:59 +0200

    Kivételkezelések

---
+++ kivetelkezeles/src/kivetelkezeles/Main.java
@@ -0,0 +1,22 @@
+package kivetelkezeles;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try {
+		//System.out.println(args[0]);
+			int a = Integer.parseInt(args[0]);
+			int b = a / 0;
+			System.out.println(b);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("\nAdj meg értéket a tömbnek!");
+			System.out.println("\n" + e);
+		} catch (ArithmeticException e) {
+			System.err.println("\nNullával osztottál!");
+			System.out.println("\n" + e);
+		}
+
+	}
+
+}
---
+++ kivetelkezeles/src/kivetelkezeles/Main2.java
@@ -0,0 +1,10 @@
+package kivetelkezeles;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+			
+		
+	}
+
+}
