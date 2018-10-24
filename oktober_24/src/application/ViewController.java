c466d280166028806517fb3d5a0a2a33c8e5e59e
Vincze Szilvia <sz1lv.vincze@gmail.com>
2018-10-24 16:42:09 +0200

    Táblázat második próba

---
+++ oktober_24/src/application/ViewController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TextField idMezo;
+
+    @FXML
+    private TextField szuletesidatumMezo;
+
+    @FXML
+    private TextField keresztnevMezo;
+
+    @FXML
+    private TextField vezeteknevMezo;
+
+    @FXML
+    void hozzaadGomb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuItemKilepes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuItemNevjegy(ActionEvent event) {
+
+    }
+
+}
