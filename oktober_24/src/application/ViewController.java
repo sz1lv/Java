dbd0b9be8a9a0469c243a583d4904aac1806d5e1
Vincze Szilvia <sz1lv.vincze@gmail.com>
2018-10-24 17:52:13 +0200

    TextField működik

--- oktober_24/src/application/ViewController.java
+++ oktober_24/src/application/ViewController.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{ //az initet importálni, majd a viewcontrollert add ... method
 
     @FXML
     private TableView<?> tabla;
@@ -24,17 +29,27 @@ public class ViewController {
 
     @FXML
     void hozzaadGomb(ActionEvent event) {
-
+    	//System.out.println("MŰKÖDIK!");
+    	System.out.println(idMezo.getText()); //Konzolon lecsekkoljuk, működnek-e a filedek
+    	System.out.println(vezeteknevMezo.getText());
+    	System.out.println(keresztnevMezo.getText());
+    	System.out.println(szuletesidatumMezo.getText());
     }
 
     @FXML
     void menuItemKilepes(ActionEvent event) {
-
+    	Platform.exit(); //fájl --> kilépés
     }
 
     @FXML
     void menuItemNevjegy(ActionEvent event) {
 
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
